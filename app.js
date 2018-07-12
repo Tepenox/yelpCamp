05a47cd16bf7822cf856b6023e07b8b86e8a1780
ANASS-LAP\Hp <anasselafyabusniess@gmail.com>
2018-07-12 20:10:04 +0100

    creat comments routes and 2 seperate folders for different routes

--- app.js
+++ app.js
@@ -16,55 +16,89 @@ seedDB();
 
 
 app.get("/", function (req, res) {
-    res.redirect("/campgrounds");
+    res.render("landing");
 });
-
+//INDEX
 app.get("/campgrounds", function (req, res) {
     Campground.find({}, function (err, allCampgrounds) {
         if (err) {
             console.log(err);
 
         } else {
-            res.render("index", { campgrounds: allCampgrounds });
+            res.render("campgrounds/index", { campgrounds: allCampgrounds });
         }
 
     })
 
 
 })
-
+//NEW
 app.get("/campgrounds/new", function (req, res) { //be carefull here the SHOW route will be triguered first if we did putit above tis one so it s a good thing to keep it here
-    res.render("new");
+    res.render("campgrounds/new");
 
 })
-
+//CREAT
 app.post("/campgrounds", function (req, res) {
     var newCampground = req.body.campground;
-    
-    Campground.create(newCampground, function(err,newlyCreated){
+
+    Campground.create(newCampground, function (err, newlyCreated) {
         if (err) {
             console.log(err);
-            
-        }else {
+
+        } else {
             res.redirect("/campgrounds");
         }
     })
-    
+
 })
 
-
-app.get("/campgrounds/:id",function(req,res){
+//SHOW
+app.get("/campgrounds/:id", function (req, res) {
     Campground.findById(req.params.id).populate("comments").exec(function (err, foundCampground) {
         if (err) {
             res.redirect("/");
-        }else{
-            res.render("show",{campground:foundCampground});
+        } else {
+            res.render("campgrounds/show", { campground: foundCampground });
         }
     })
 })
 
+//=============
+//COMMENTS ROUTES
+//=============
 
+//NEW
+app.get("/campgrounds/:id/comments/new", function (req, res) {
+    Campground.findById(req.params.id, function (err, foundCampground) {
 
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("comments/new", { campground: foundCampground });
+        }
+    })
+})
+
+//CREAT
+
+app.post("/campgrounds/:id/comments", function (req, res) {
+    Campground.findById(req.params.id, function (err, foundCampground) {
+        if (err) {
+            console.log(err);
+            res.redirect("/campgrounds");
+        } else {
+            Comment.create(req.body.comment , function(err,comment){
+                if (err) {
+                    console.log(err);
+                }else{
+                    foundCampground.comments.push(comment); // foundCampground and not Campground
+                    foundCampground.save(); // important one i always forget it  :p
+                    res.redirect("/campgrounds/"+req.params.id);
+                }
+            })
+        }
+    })
+})
 
 
 app.listen(3000, function () {
